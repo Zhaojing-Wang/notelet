8d725bb19e0b5f832c393e42ce1a3cd3e3964578
Zhaojing Wang <zhaojing.wzj@gmail.com>
2026-04-25 14:02:56 +0800

    Initial Commit

---
+++ notelet/noteletApp.swift
@@ -0,0 +1,32 @@
+//
+//  noteletApp.swift
+//  notelet
+//
+//  Created by Zhaojing Wang on 4/25/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct noteletApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ noteletTests/noteletTests.swift
@@ -0,0 +1,18 @@
+//
+//  noteletTests.swift
+//  noteletTests
+//
+//  Created by Zhaojing Wang on 4/25/26.
+//
+
+import Testing
+
+struct noteletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
